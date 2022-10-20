5b559bf351882399b6b645f3021e4fc5976874ac
DeeeAich <donnachadavylohan@gmail.com>
2022-10-20 11:04:21 +0100

    Added Blight to the game

--- Farmvillians Prototype/Assets/FarmPlot.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FarmPlot : MonoBehaviour
-{
-    public GameObject seeded;
-    public GameObject grown;
-}
